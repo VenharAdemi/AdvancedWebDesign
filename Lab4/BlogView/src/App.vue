e5b4462c3c05be7671f37483629765fa6ad4b21a
VenharAdemi <venhar.adm7@gmail.com>
2025-01-24 22:08:53 +0100

    FinalResult

--- Lab4/BlogView/src/App.vue
+++ Lab4/BlogView/src/App.vue
@@ -1,11 +1,22 @@
-<script setup>
-
-</script>
-
 <template>
- 
+  <BlogView />
 </template>
 
-<style>
+<script setup>
+import BlogView from "./components/BlogView.vue";
+</script>
 
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto 0;
+  max-width: 800px;
+}
+input {
+  padding: 6px;
+}
 </style>
